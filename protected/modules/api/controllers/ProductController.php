2ec6b6f7f4737969ab8259fa4881c2c3751f0151
ztenst <754710007@qq.com>
2018-11-12 10:05:58 +0800

    th

--- protected/modules/api/controllers/ProductController.php
+++ protected/modules/api/controllers/ProductController.php
@@ -374,12 +374,21 @@ class ProductController extends ApiController
     	$imgs = isset($arrs['images'])?$arrs['images']:[];
     	unset($arrs['images']);
     	if(isset($arrs['id'])&&$arrs['id']) {
+    		if($title = $arrs['title']) {
+    			if(Yii::app()-db->createCommand("select id from product where title='$title' and id!=".$arrs['id'])->queryScalar()){
+    				return $this->returnError("已有该商品名，请勿重复");
+    			}
+    		}
     		$obj = ProductExt::model()->findByPk($arrs['id']);
     	} else {
+    		if(Yii::app()-db->createCommand("select id from product where title='$title'")->queryScalar()){
+    				return $this->returnError("已有该商品名，请勿重复");
+    			}
     		$obj = new ProductExt;
     	}
     	$obj->attributes = $arrs;
     	$obj->status = 0;
+
     	if($obj->save()) {
     		Yii::app()->db->createCommand("delete from album where pid=".$obj->id." and type=1")->execute();
     		// AlbumExt::model()->deteleAllByAttributes(['pid'=>$arrs['id'],'type'=>1]);
